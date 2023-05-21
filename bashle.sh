9777c2060b5762a26fb10ccbfab68aeeb511c128
Justin Rabe <jtr342@gmail.com>
2023-05-21 14:26:46 -0700

    inserting contents of words.txt into an array

--- bashle.sh
+++ bashle.sh
@@ -1,6 +1,9 @@
 # /bin/bash
 
 echo "Welcome to Bashle!"
+filename=words.txt
+declare -a myArray
+mapfile -t myArray < filename
 for i in {5..1}
 do
 	echo "Please enter a 5 letter word. you have $i tries remaining."
