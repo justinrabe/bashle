c6828b4e4c676d7baa359f05b4c34115634a347b
justin <jtr342@gmail.com>
2023-05-21 23:33:25 -0400

    created variable for randomly selected word from list

--- bashle.sh
+++ bashle.sh
@@ -1,9 +1,12 @@
 # /bin/bash
 
 echo "Welcome to Bashle!"
-filename=words.txt
 declare -a myArray
-mapfile -t myArray < filename
+mapfile -t myArray < words.txt
+rand=$[$RANDOM % ${#myArray[@]}]
+ans=${myArray[rand]}
+
+echo $ans
 for i in {5..1}
 do
 	echo "Please enter a 5 letter word. you have $i tries remaining."
