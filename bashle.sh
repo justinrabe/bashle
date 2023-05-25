55782aea7bc63baf8733063a3f60ee1dad4d68cb
justin <jtr342@gmail.com>
2023-05-25 00:32:37 -0400

    figuring out how to match each letter

--- bashle.sh
+++ bashle.sh
@@ -12,9 +12,10 @@ do
 	echo "Please enter a 5 letter word. you have $i tries remaining."
 	read guess
 	for i in $(seq 1 ${#guess}); do
-		echo "${guess:i-1:1}"
 		for j in $(seq 1 ${#ans}); do
-			echo "${ans:j-1:1}"
+			if [ ${guess:i-1:1} == ${ans:j-1:1} ]; then
+				echo "green match"
+			fi				
 		done
 	done
 done
